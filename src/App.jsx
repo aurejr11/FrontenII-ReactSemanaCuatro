d089d8646985407ae535f069b6fb099f6de7ced0
@Apartamento26 <desarrollo@academicos.cesde.edu.co>
2026-02-18 08:53:26 -0500

    aureliouno

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/* se deja asi */
+
+import './App.css'
+import Services from './pages/Services'
+function App() {
+  
+
+  return (
+    <>
+      <Services/>
+    </>
+  )
+}
+
+export default App
+
+
+
+
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+
+//funcion
+
+function Sidebar () {
+    return(
+        <aside>
+            <img src="" alt="Logo" />
+            <nav>
+                <a href="">Servicios</a>
+                <a href="">Contenidos</a>
+                <a href="">Redes</a>
+            </nav>
+        </aside>
+
+
+    )
+
+}
+
+export default Sidebar
