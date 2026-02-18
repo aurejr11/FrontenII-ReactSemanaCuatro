9c2e42ba203e8886a9c778fa81ddb5ed160d303b
VELASQUEZ <aurejr11@gmail.com>
2026-02-18 13:12:17 -0500

    Descripción de tus cambios

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -7,11 +7,8 @@ function Services (){
     return(
         <main>
             <Sidebar/>
-<<<<<<< HEAD
+
             <section className = "container">
-=======
-            <section class name = "container">
->>>>>>> d089d8646985407ae535f069b6fb099f6de7ced0
                 <h1>Servicios</h1>
                 <section>
                     <Card/>
