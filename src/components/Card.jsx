7b9c628072cec49ed2df8f746273fdbfdd8c3845
VELASQUEZ <aurejr11@gmail.com>
2026-02-18 11:15:33 -0500

    mis cambios

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+
+// funcion
+
+function Card () {
+   // return{} retorno javascript por las llaves
+
+    return ( /* retorno html por los parantesis*/
+        <article>
+
+            <section></section>
+            <section>
+                <h3>Titulo</h3>
+            </section>
+
+
+        </article>
+     )
+
+}
+
+export default Card
---
+++ src/pages/Services.jsx
@@ -0,0 +1,24 @@
+
+import Sidebar from '../components/Sidebar'
+import Card from '../components/Card'
+import Footer from '../components/Footer'
+
+function Services (){
+    return(
+        <main>
+            <Sidebar/>
+            <section className = "container">
+                <h1>Servicios</h1>
+                <section>
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                </section>
+                <Footer/>
+            </section>
+        </main>
+    );
+}
+
+export default Services
+
